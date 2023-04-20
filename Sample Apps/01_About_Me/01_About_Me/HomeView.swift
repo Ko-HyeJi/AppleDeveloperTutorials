06f87ba3d332afcde74141fd37c01010a97efdb1
HYEJI KO <hy2ji.k5@gmail.com>
2023-04-20 19:19:23 +0900

    [fixed] HomeView
    Changed some details

--- Sample Apps/01_About_Me/01_About_Me/HomeView.swift
+++ Sample Apps/01_About_Me/01_About_Me/HomeView.swift
@@ -14,14 +14,17 @@ struct HomeView: View {
                 .font(.largeTitle.italic())
                 .fontWeight(.bold)
 
-            Image(info.image)
-                .resizable()
-                .frame(maxWidth: 300, maxHeight: 300)
-                .aspectRatio(contentMode: .fit)
-                .cornerRadius(1000)
-                .padding([.top, .bottom], 50)
-                .overlay(Circle()
-                    .stroke(.black, lineWidth: 1))
+            ZStack {
+                Circle()
+                    .foregroundColor(lightGray)
+                    .frame(width: 300, height: 300)
+                Image(info.image)
+                    .resizable()
+                    .frame(maxWidth: 300, maxHeight: 300)
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(1000)
+                    .padding([.top, .bottom], 50)
+            }
 
             Text(info.name)
                 .font(.largeTitle.bold().italic())
