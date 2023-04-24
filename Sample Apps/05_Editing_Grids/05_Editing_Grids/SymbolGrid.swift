0c3087fcfaa8f788f3608ef18aa476497e397a12
HYEJI KO <hy2ji.k5@gmail.com>
2023-04-24 21:55:36 +0900

    Changed Project Name

---
+++ Sample Apps/05_Editing_Grids/05_Editing_Grids/SymbolGrid.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  05_Editing_Grids
+//
+//  Created by 고혜지 on 2023/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
