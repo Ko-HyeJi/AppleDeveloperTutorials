c7459f18c3fc0966f355cec8191e6a08cdefc2c6
HYEJI KO <hy2ji.k5@gmail.com>
2023-06-01 16:20:23 +0900

    :tada: create project file

---
+++ Sample Apps/03_Date_Planner/03_Date_Planner/DatePlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DatePlannerApp.swift
+//  03_Date_Planner
+//
+//  Created by 고혜지 on 2023/06/01.
+//
+
+import SwiftUI
+
+@main
+struct _3_Date_PlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
